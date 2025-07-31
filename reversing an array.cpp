c8dc079431439f7538b9e318354136ca6706ab57
Pal Jain <paljaiin@gmail.com>
2025-07-31 11:19:37 +0530

    Create reversing an array.cpp

---
+++ reversing an array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int arr[6];
+int reverse_array[6];
+cout<<"The numbers of array are:"<<endl;
+
+for(int i=0; i<6; i++){
+    cin>>arr[i];
+}
+
+for(int n=0; n<6; n++){
+    cout<<arr[n]<<" ";
+}
+cout<<"\nReversed array is:";
+for(int i=5; i>=0; i--){
+    cout<<arr[i]<<" ";
+}
+
+   return 0;
+}
