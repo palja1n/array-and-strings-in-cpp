139d29420012e06fd3e657de9d6b6794e128e05f
Pal Jain <paljaiin@gmail.com>
2025-07-31 11:12:57 +0530

    Create different syntax for displaying array.cpp

---
+++ different syntax for displaying array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str1[4] = "C++";
+    char str2[] = {'C','+','+','\0'};
+    char str3[4] = {'C','+','+','\0'};
+    string str4 = "C++";
+
+    cout << "str1: " << str1 << endl;
+    cout << "str2: " << str2 << endl;
+    cout << "str3: " << str3 << endl;
+    cout << "str4: " << str4 << endl;
+
+    return 0;
+}
