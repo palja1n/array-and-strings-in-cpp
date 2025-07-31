ce462215b77fd22984a158c4e65176b0d6b4bb7e
Pal Jain <paljaiin@gmail.com>
2025-07-31 11:42:13 +0530

    Update reversing an array.cpp

--- reversing an array.cpp
+++ reversing an array.cpp
@@ -13,10 +13,18 @@ for(int i=0; i<6; i++){
 for(int n=0; n<6; n++){
     cout<<arr[n]<<" ";
 }
-cout<<"\nReversed array is:";
+cout<<"\nReversed array is: ";
 for(int i=5; i>=0; i--){
     cout<<arr[i]<<" ";
 }
 
    return 0;
 }
+
+/*
+Output:
+The numbers of array are:
+3 4 5 8 9 8
+3 4 5 8 9 8 
+Reversed array is: 8 9 8 5 4 3
+    */
